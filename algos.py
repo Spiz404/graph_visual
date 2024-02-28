3f8b3177f687a1e0762b91144b7df11c589be53c
Spiz404 <lorespiz03@gmail.com>
2024-02-28 21:21:23 +0100

    removing some print

--- algos.py
+++ algos.py
@@ -14,9 +14,8 @@ def bfs(source : Node, graph : Graph):
         lab = w.popleft()
         if lab not in visited:
             visited.append(lab)
-        print(visited)
-        for element in graph[str(lab)]:
-            print(str(element["node"].getLabel()) + " " + str(element["node"] not in visited))
+   
+        for element in graph[str(lab)]:  
             if element["node"] not in visited:
                 visited.append(element["node"]) 
                 w.append(element["node"])
@@ -24,8 +23,6 @@ def bfs(source : Node, graph : Graph):
                 vl.append(link)
               
 
-    print("lista visitati" + str(visited))
-    print("links " + str(vl))
     return Graph(visited, vl)
 
 def dfs(source : Node, graph : Graph):
